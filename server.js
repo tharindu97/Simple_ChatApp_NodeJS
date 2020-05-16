1c4454fe846d3ffa0b9f78828f3282419410449c
tharindu97 <tharindukavishna@gmail.com>
2020-05-16 10:17:14 +0530

    project_start_server_create

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
