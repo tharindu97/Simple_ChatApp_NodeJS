b25bacc370f53e2b8af24bcc1b8482fc3621678c
tharindu97 <tharindukavishna@gmail.com>
2020-05-17 17:34:55 +0530

    have_bugs

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...');
+    //socket.emit('message', 'Welcome to chatCord!');
+});
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
