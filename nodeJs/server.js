f5d443cf5193d43176a8f472041441902e121fdc
tharindu97 <tharindukavishna@gmail.com>
2020-05-18 22:31:38 +0530

    init

--- nodeJs/server.js
+++ nodeJs/server.js
@@ -16,7 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', socket => {
     console.log('New WS Connection...');
+
+    // Welcome current user
     socket.emit('message', 'Welcome to chatCord!');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    //Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
 });
 
 
