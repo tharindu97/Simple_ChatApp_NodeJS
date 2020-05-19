7372889d67ade89f89077e0b595e99a4e8b5a220
tharindu97 <tharindukavishna@gmail.com>
2020-05-19 11:25:18 +0530

    init_next

--- nodeJs/public/js/main.js
+++ nodeJs/public/js/main.js
@@ -1,4 +1,5 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
@@ -6,6 +7,9 @@ const socket = io();
 socket.on('message', message =>{
     console.log(message);
     outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //Meeting submit
@@ -17,6 +21,10 @@ chatForm.addEventListener('submit', e =>{
 
     // Emit message to server
     socket.emit('chatMessage', msg);
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
 
 // Output message to DOM
--- nodeJs/server.js
+++ nodeJs/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const PORT = 3000 || process.env.PORT;
 
@@ -13,26 +14,30 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when client connects
 io.on('connection', socket => {
     console.log('New WS Connection...');
 
     // Welcome current user
-    socket.emit('message', 'Welcome to chatCord!');
+    socket.emit('message', formatMessage(botName,'Welcome to chatCord!'));
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName,'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER',msg));
     });
 });
 
 
 server.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
+
+//32 43
